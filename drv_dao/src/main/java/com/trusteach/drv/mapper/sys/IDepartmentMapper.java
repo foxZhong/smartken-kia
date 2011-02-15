459d1aa626eb1cff016d6f6df271bad628348180
smartken0824 <smartken0824@gmail.com>
2011-02-15 13:38:55 +0000

    dao

---
+++ drv_dao/src/main/java/com/trusteach/drv/mapper/sys/IDepartmentMapper.java
@@ -0,0 +1,7 @@
+package com.trusteach.drv.mapper.sys;
+
+import com.smartken.kia.core.model.IMapper;
+
+public interface IDepartmentMapper<T> extends IMapper<T>{
+
+}
---
+++ drv_dao/src/main/java/com/trusteach/drv/mapper/sys/IDictMapper.java
@@ -0,0 +1,7 @@
+package com.trusteach.drv.mapper.sys;
+
+import com.smartken.kia.core.model.IMapper;
+
+public interface IDictMapper<T> extends IMapper<T> {
+
+}
