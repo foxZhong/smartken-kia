d7421d5a2631e12ef29676db85bf5d2f06a9638c
smartken0824 <smartken0824@gmail.com>
2011-07-12 08:32:09 +0000

    移除SessionRequiredFilter；增加VerifyFilter，实现更灵活的验证配置

--- kia-core/src/main/java/com/smartken/kia/core/filter/VerifyFilter.java
+++ kia-core/src/main/java/com/smartken/kia/core/filter/VerifyFilter.java
@@ -1,41 +1,24 @@
 package com.smartken.kia.core.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.oro.text.regex.PatternMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.sun.tools.javac.resources.compiler;
+public class VerifyFilter extends OncePerRequestFilter{
 
-
-public  class SessionRequiredFilter extends OncePerRequestFilter  {
-
-	public static String AUTH_USER="authUser";
-
-	private String[] requiredKeys=new String[]{};
+	private ArrayList<IVerifyRule> rules=new ArrayList<IVerifyRule>();
 	private String[] includeRegexps=new String[]{};
 	private String[] excludeRegexps=new String[]{};
 	private String errorPath="";
-	 
 	
-	
-
-	public void setRequiredKeys(String requiredKeys) {
-		String[] vals=requiredKeys.split(",");
-		this.requiredKeys = vals;
-	}
-
 	public void setIncludeRegexps(String includeRegexp) {
 		String[] vals=includeRegexp.split(",");
 		this.includeRegexps = vals;
@@ -49,16 +32,30 @@ public  class SessionRequiredFilter extends OncePerRequestFilter  {
 	public void setErrorPath(String errorPath) {
 		this.errorPath = errorPath;
 	}
-
-	public void destroy() {
-		// TODO Auto-generated method stub
-
+	
+	public void setRules(String validates){
+		String[] vals=validates.split(",");
+		for(String val :vals){
+			try {
+				Class cls=Class.forName(val);
+				IVerifyRule rule=(IVerifyRule) cls.newInstance();
+				rules.add(rule);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
-
-
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest req,
-			HttpServletResponse res, FilterChain fc)
+			HttpServletResponse res, FilterChain filterChain)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		 String url=req.getServletPath();
@@ -81,26 +78,14 @@ public  class SessionRequiredFilter extends OncePerRequestFilter  {
 			 }
 		 }
 		 if(isMatch){
-			 for(String key :requiredKeys){
-	        	 if(req.getSession().getAttribute(key)==null){
+			 for(IVerifyRule rule :rules){
+	        	 if(!rule.doVerifyRule(req, res)){
 	        		 res.sendRedirect(basePath+errorPath);
+	        		 break;
 	        	 }
 	         }
 		 }
-		 fc.doFilter(req, res);
-	}
-	
-
-	
-	public static void main(String[] args){
-		 String regexp="(/to/).*(.action)$";
-		 String url="/admin/Buslog/to/count.action";
-		 Pattern p=Pattern.compile(regexp);
-		 Matcher matcher=p.matcher(url);
-		 boolean isMatch= matcher.find();
-		 System.out.println(isMatch);
-		 //int end= matcher.end();
-		 //System.out.println(end);
+		 filterChain.doFilter(req, res);
 	}
 
 }
